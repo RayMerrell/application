312d4463ef6d8a0160f99412c1ebf292db8edd3d
Ray Merrell <RayMerrell@outlook.com>
2023-04-25 13:58:38 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from 'react-bootstrap/Spinner';
 import Badge from 'react-bootstrap/Badge';
 
 function App() {
   return (
     <div className="App">
+
        <Spinner animation="border" variant="danger" />
+       <br />
        <Badge bg="secondary">Hello World</Badge>
+
     </div>
   );
 }
