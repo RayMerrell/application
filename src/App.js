0cdfb36d300ce2c0942d9dcfe27bce781b97a84b
Ray Merrell <RayMerrell@outlook.com>
2023-04-25 13:52:12 +0100

    M46 REACT REST

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Spinner from 'react-bootstrap/Spinner';
+import Badge from 'react-bootstrap/Badge';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Spinner animation="border" variant="danger" />
+       <Badge bg="secondary">Hello World</Badge>
     </div>
   );
 }
